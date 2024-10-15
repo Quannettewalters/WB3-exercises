f8ab54a8a982f0dd569cf8490229d12f5721689e
Quannette Walters <quannette1@gmail.com>
2024-10-15 15:02:32 -0400

    exercise 1

---
+++ Functions/simple_functions.js
@@ -0,0 +1,17 @@
+function favoriteThings(){
+    console.log("My name is Quannette.")
+    console.log("My favorite movie is White Chicks")
+    console.log("My facorite artist is Sza")
+}
+
+function whyImHere(){
+    console.log("My dream is to develope software that contribute to the evolution of technology.")
+}
+
+function favoritePlace(){
+    console.log("My favorite place to visit is the Vickery Creek Waterfalls, because I love nature! ")
+}
+
+favoriteThings();
+whyImHere();
+favoritePlace();
